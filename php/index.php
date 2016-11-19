c46f66d35e9f118e6bb295ae805574c4fe22796d
Chloe Silver <chloe@eventmobi.com>
2016-11-19 12:21:40 -0500

    Moved php, added index.html

---
+++ php/index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <!-- Favicon -->
+  <link href="http://thoughtsvisualized.blogspot.ca/favicon.ico" rel="icon" type="image/x-icon">
+
+  <!-- Data Stuff -->
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Chloe Silver Design</title>
+  <meta name="author" content="Chloe Silver" />
+  <meta name="description" content="Chloe Silver's Design Portfolio" />
+  <meta name="keywords"  content="design,portfolio,creative,freelance,toronto,animate,code,illustration,installation,craft,print,web" />
+  <meta name="Resource-type" content="Document" />
+  <meta name=viewport
+  content="width=device-width, initial-scale=1.0, minimum-scale=0.5 maximum-scale=1.0">
+
+  <!-- CSS -->
+  <!-- Carousel -->
+  <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+  <!-- Main page CSS -->
+  <link rel="stylesheet" type="text/css" href="css/styles.css" />
+  <!-- Fonts -->
+  <link href='https://fonts.googleapis.com/css?family=Judson:400,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Lato:400,300,900,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="../css/stylesheet.css" type="text/css" charset="utf-8" />
+
+  <!-- Scripts -->
+  <!-- jQuery -->
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script><script src="http://code.jquery.com/jquery-latest.min.js"></script>
+  <script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <!-- Carousel -->
+  <script type="text/javascript" src="slick/slick.min.js"></script>
+  <!-- Carousel - mods -->
+  <script type="text/javascript" src="slick/slicky.js"></script>
+  <!-- Sliding Nav -->
+  <script type="text/javascript" src="js/head.js"></script>
+
+</head>
+<body>
+
+  <?php include("header.html"); ?>
+
+  <section class="top">
+    <div>
+      <div class="carousel">
+        <div><a href="causes"><img src="img/caro2.png" class="caroclass"></a></div>
+        <div><a href="beef"><img src="img/caro1.png" class="caroclass"></a></div>
+        <div><a href="expo2025"><img src="img/caro3.png" class="caroclass"></a></div>
+        <div><a href="desire"><img src="img/caro4.png" class="caroclass"></a></div>
+      </div>
+    </div>
+  </section>
+
+  <section class="work" id="work">
+    <div id="contain">
+      <div id="dummy"></div>
+      <div class="element beef">
+        <a href="beef" style="top: 1%; left: 0.5%;width:90%;height: 90%;display: inline-block;" align="center">
+          <br><br>What's Your Beef<br>
+          Digital and Print Brochure
+          <br><br><div class="view">VIEW PROJECT</div>
+        </a>
+      </div>
+    </div>
+    <div id="contain">
+        <div id="dummy"></div>
+        <div class="element revo"></div>
+        <a href="revolution" style="top: 1%; left: 0.5%;width:90%;height: 90%;display: inline-block;" align="center"><br><br>
+          rEVOLution<br>
+        Event Branding
+        <br><br><div class="view">VIEW PROJECT</div></a>
+    </div>
+    <div id="contain">
+        <div id="dummy"></div>
+        <div class="element days">
+        <a href="21days" style="top: 1%; left: 0.5%;width:90%;height: 90%;display: inline-block;" align="center"><br><br>
+          21 Days<br>
+        Mobile App Design
+        <br><br><div class="view">VIEW PROJECT</div></a>
+        </div>
+    </div>
+    <div id="contain">
+        <div id="dummy"></div>
+        <div class="element bean">
+        <a href="flora" style="top: 1%; left: 0.5%;width:90%;height: 90%;display: inline-block;" align="center"><br><br>
+          Flora & Fauna<br>
+        Material Design
+        <br><br><div class="view">VIEW PROJECT</div></a>
+        </div>
+    </div>
+    <div id="contain">
+        <div id="dummy"></div>
+        <div class="element desire">
+        <a href="desire" style="top: 1%; left: 0.5%;width:90%;height: 90%;display: inline-block;" align="center"><br><br>
+          Objects of Desire<br>
+        Experimental Web Design
+        <br><br><div class="view">VIEW PROJECT</div></a>
+        </div>
+    </div>
+    <div id="contain">
+        <div id="dummy"></div>
+        <div class="element mag">
+        <a href="upfront" style="top: 1%; left: 0.5%;width:90%;height: 90%;display: inline-block;" align="center"><br><br>
+          Upfront Mag<br>
+        Editorial Design
+        <br><br><div class="view">VIEW PROJECT</div></a>
+        </div>
+    </div>
+    <div id="contain">
+        <div id="dummy"></div>
+        <div class="element causes">
+        <a href="causes" style="top: 1%; left: 0.5%;width:90%;height: 90%;display: inline-block;" align="center"><br><br>
+          Downfall By Numbers<br>
+        Interactive Data Visualization
+        <br><br><div class="view">VIEW PROJECT</div></a>
+        </div>
+    </div>
+    <div id="contain">
+        <div id="dummy"></div>
+        <div class="element hear">
+        <a href="hearnow" style="top: 1%; left: 0.5%;width:90%;height: 90%;display: inline-block;" align="center"><br><br>
+          Hear & Now<br>
+        Mobile App Design
+        <br><br><div class="view">VIEW PROJECT</div></a>
+        </div>
+    </div>
+    <div id="contain">
+        <div id="dummy"></div>
+        <div class="element fav">
+        <a href="favethings" style="top: 1%; left: 0.5%;width:90%;height: 90%;display: inline-block;" align="center"><br><br>
+          Favourite Things<br>
+        Experimental Print Design
+        <br><br><div class="view">VIEW PROJECT</div></a>
+        </div>
+    </div>
+    <div id="contain">
+        <div id="dummy"></div>
+        <div class="element expo">
+        <a href="expo2025" style="top: 1%; left: 0.5%;width:90%;height: 90%;display: inline-block;" align="center"><br><br>
+          Expo 2025<br>
+        Event Branding
+        <br><br><div class="view">VIEW PROJECT</div></a>
+        </div>
+    </div>
+    <div id="contain">
+        <div id="dummy"></div>
+        <div class="element inuitart">
+        <a href="inuitart" style="top: 1%; left: 0.5%;width:90%;height: 90%;display: inline-block;" align="center"><br><br>
+          Cultural Networks<br>
+        Poster with Augmented Reality
+        <br><br><div class="view">VIEW PROJECT</div></a>
+        </div>
+    </div>
+    <div id="contain">
+        <div id="dummy"></div>
+        <div class="element fgs">
+        <a href="fgs" style="top: 1%; left: 0.5%;width:90%;height: 90%;display: inline-block;" align="center"><br><br>
+          FGS Turns 50<br>
+        Editorial and Installation Design
+        <br><br><div class="view">VIEW PROJECT</div></a>
+        </div>
+    </div>
+  </section>
+
+  <?php include("footer.html"); ?>
+</body>
+
+<?php include("scripts.html"); ?>
+</html>
